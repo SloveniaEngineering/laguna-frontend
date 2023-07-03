13fdd4d8139816454f648a8eff65901a01eaef55
zigapovhe <ziga@povhe.si>
2023-07-03 23:15:43 +0200

    adds riverpod as state management library

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,1 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:laguna/constants.dart';
+import 'package:laguna/ui/loginScreen.dart';
 
+final routerProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: Constants.loginRoute,
+    routes: [
+      GoRoute(
+        path: Constants.loginRoute,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: Constants.registerRoute,
+        builder: (context, state) => const LoginScreen(), //Change this to RegisterScreen() when implemented
+      ),
+      GoRoute(
+        path: Constants.forgotPasswordRoute,
+        builder: (context, state) => const LoginScreen(), //Change this to ForgottenPassScreen() when implemented
+      ),
+    ],
+  );
+});
